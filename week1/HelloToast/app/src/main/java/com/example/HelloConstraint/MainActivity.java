36c1b4b18b53938968341fd35fd8764945355ae4
Sagar Khadka <breakdowns.blasts@gmail.com>
2019-02-17 22:31:37 +0545

    Homework-The layout editor

--- week1/HelloToast/app/src/main/java/com/example/HelloConstraint/MainActivity.java
+++ week1/HelloToast/app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.hellotoast;
+package com.example.HelloConstraint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    Button btn_Zero;
 
 
     @Override
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        btn_Zero = (Button) findViewById(R.id.btn_Zero);
+
     }
 
     public void showToast(View view) {
@@ -29,5 +33,12 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void putZero(View view){
+        mShowCount.setText(""+0);
+        btn_Zero.setBackgroundColor(getResources().getColor(android.R.color.darker_gray
+        ));
+
     }
 }
